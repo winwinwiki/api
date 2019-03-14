ca7bc284982ce8c0b1b6c7ef0216af0c1edb9293
Sunny Tambi <sunnytambi@gmail.com>
2019-03-14 19:19:34 +0530

    adding codedeploy script

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# sudo yum install wget -y
+# wget --no-cookies --no-check-certificate --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/11.0.2+9/f51449fcd52f4d52b93a989c5c56ed3c/jdk-11.0.2_linux-x64_bin.rpm
+# rpm -Uvh jdk-11.0.2_linux-x64_bin.rpm
+# sudo rpm -Uvh jdk-11.0.2_linux-x64_bin.rpm
---
+++ scripts/stop_server.sh
@@ -0,0 +1,1 @@
+service winwin-service stop
