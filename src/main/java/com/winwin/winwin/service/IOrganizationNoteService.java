d984a089106a92d768815f8f7e5a0695b1564cbe
Arvind Khatik <ArvindK@talentica-all.com>
2019-04-04 19:23:05 +0530

    changes added for org details v17

---
+++ src/main/java/com/winwin/winwin/service/IOrganizationNoteService.java
@@ -0,0 +1,19 @@
+package com.winwin.winwin.service;
+
+import java.util.List;
+
+import com.winwin.winwin.entity.OrganizationNote;
+import com.winwin.winwin.payload.OrganizationNotePayload;
+
+/**
+ * @author ArvindKhatik
+ *
+ */
+public interface IOrganizationNoteService {
+	OrganizationNote createOrganizationNote(OrganizationNotePayload organizationNotePayload);
+
+	void removeOrganizationNote(Long noteId);
+
+	List<OrganizationNote> getOrganizationNoteList(Long id);
+
+}
