ac18305d22a8b31aba724e1db04a76a78d216eae
Arvind Khatik <ArvindK@talentica-all.com>
2019-04-15 15:15:11 +0530

    changes added for org details v19.2

--- src/main/java/com/winwin/winwin/service/IUserService.java
+++ src/main/java/com/winwin/winwin/service/IUserService.java
@@ -15,10 +15,18 @@ public interface IUserService {
 	public void createUser(UserPayload payload) throws UserException;
 
 	public UserPayload getUserInfo(String userName) throws UserException;
-	
+
 	public List<UserPayload> getUserList() throws UserException;
-	
+
 	public String getUserStatus(String userName) throws UserException;
+
+	public void resetUserPassword(UserPayload payload) throws UserException;
+
+	public void resendConfirmationCode(UserPayload payload) throws UserException;
+
+	void confirmResetPassword(UserSignInPayload payload) throws UserException;
+
+	public void changePassword(UserSignInPayload payload) throws UserException;
 
 	public AuthenticationResultType userSignIn(UserSignInPayload payload) throws UserException;
 
