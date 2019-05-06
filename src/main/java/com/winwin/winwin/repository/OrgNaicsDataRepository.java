63c97975e6a40b8d15e22e011b15dffa5ab19998
manoharkoukuntla <manoharkoukuntla3@gmail.com>
2019-05-06 14:07:41 +0530

    Added Filter for organization filter response

--- src/main/java/com/winwin/winwin/repository/OrgNaicsDataRepository.java
+++ src/main/java/com/winwin/winwin/repository/OrgNaicsDataRepository.java
@@ -1,9 +1,11 @@
 package com.winwin.winwin.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.winwin.winwin.entity.OrgNaicsData;
 
 public interface OrgNaicsDataRepository extends JpaRepository<OrgNaicsData, Long> {
-
+	List<OrgNaicsData> findByNameContainingIgnoreCase(String name);
 }
