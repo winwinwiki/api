5f58999fdd0f389ab414bc277202368a1c799c00
Arvind Khatik <arvind.khatik@talentica.com>
2019-06-26 12:47:37 +0530

    updating application start scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,1 +1,5 @@
-service winwin-service start
+#service winwin-service start
+sudo kill $(pgrep java)
+echo "Starting WinWin application"
+nohup sudo java -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
+echo "WinWin application started"
