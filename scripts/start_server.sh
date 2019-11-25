b70aade02d1fc3a1eb495e2b8336669697fade95
Arvind Khatik <arvind.khatik@talentica.com>
2019-11-25 18:20:30 +0530

    applying final fix for Code Deploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,11 +1,16 @@
-echo "Starting WinWin application"
+if pgrep -x "java" >/dev/null
+then
+    echo "Stopping WinWin application"
+	sudo kill $(pgrep java)
+fi
+
+chmod +x /home/ec2-user/winwin-0.0.1-SNAPSHOT.jar
+echo "Starting WinWin application for environment:  $WINWIN_ENV"
 if [[ "$WINWIN_ENV" = "Development" ]]; then
-	echo "Starting WinWin application for environment:  $WINWIN_ENV"
-	sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
-	echo "WinWin application started for environment:  $WINWIN_ENV"
+	nohup sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 elif [[ "$WINWIN_ENV" = "Staging" ]]; then
 	nohup sudo java -Xmx1792m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 elif [[ "$WINWIN_ENV" = "Production" ]]; then
 	nohup sudo java -Xmx3072m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 fi
-echo "WinWin application started"
+echo "WinWin application started for environment:  $WINWIN_ENV"
