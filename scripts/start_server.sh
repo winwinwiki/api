29747fc58c1afb9c6ac026c1f3bbe2c85df2033c
Arvind Khatik <arvind.khatik@talentica.com>
2019-11-25 16:52:35 +0530

    applying fix for Code Deploy v3

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,10 @@
 echo "Starting WinWin application"
 if [[ "$WINWIN_ENV" = "Development" ]]; then
-	java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
+	nohup sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 elif [[ "$WINWIN_ENV" = "Staging" ]]; then
-	java -Xmx1792m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
+	nohup sudo java -Xmx1792m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 elif [[ "$WINWIN_ENV" = "Production" ]]; then
-	java -Xmx3072m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
+	nohup sudo java -Xmx3072m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 fi
+nohup sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
 echo "WinWin application started"
