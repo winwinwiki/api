2caa6dbf9f811a63263a15b47664f6898ca3ab95
Arvind Khatik <arvind.khatik@talentica.com>
2019-11-27 11:37:19 +0530

    applying final fix for Code Deploy v8.4

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,7 +2,7 @@ chmod 777 /home/ec2-user/winwin-0.0.1-SNAPSHOT.jar
 
 if [[ "$WINWIN_ENV" = "Development" ]]; then
 	echo "Starting WinWin application for environment: $WINWIN_ENV"
-	nohup sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar
+	sudo java -Xmx768m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar
 	echo "WinWin application started for environment: $WINWIN_ENV"
 elif [[ "$WINWIN_ENV" = "Staging" ]]; then
 	nohup sudo java -Xmx1792m -Dserver.port=80 -jar winwin-0.0.1-SNAPSHOT.jar /tmp 2>> /dev/null >> /dev/null &
